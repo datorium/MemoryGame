7e3eaaab9dba51bf736494c8316a4d9b72dcd245
Datorium <hello@datorium.lv>
2020-05-18 16:41:12 +0300

    version 16:41

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,33 @@ namespace MemoryGame
         public Form1()
         {
             InitializeComponent();
+            InitializeGrid();
         }
+
+        private void InitializeGrid()
+        {
+            Label lable;
+            
+            Grid.BackColor = Color.LightSteelBlue;
+            Grid.CellBorderStyle = TableLayoutPanelCellBorderStyle.Outset;
+            //Webdings font
+            //e, d, k, b, N, !, &, $
+
+            for (int c = 0; c < 4; c++)
+            {
+                for(int r = 0; r < 4; r++)
+                {
+
+                }
+            }
+
+
+            //Grid.Controls.Add(new Label { Text = "Type:", Anchor = AnchorStyles.Left, AutoSize = true }, 0, 0);
+            //Grid.Controls.Add(new Label { Text = "Type:", Anchor = AnchorStyles.Left, AutoSize = true }, 0, 1);
+            //Grid.Controls.Add(new Label { Text = "Type:", Anchor = AnchorStyles.Left, AutoSize = true }, 1, 0);
+            //Grid.Controls.Add(new Label { Text = "Type:", Anchor = AnchorStyles.Left, AutoSize = true }, 3, 3);
+
+        }
+
     }
 }
