e958767d7c4544859c06954abdaaa0641a585ed8
Datorium <hello@datorium.lv>
2020-05-18 16:58:29 +0300

    version 16:58

--- Form1.cs
+++ Form1.cs
@@ -16,31 +16,39 @@ namespace MemoryGame
         {
             InitializeComponent();
             InitializeGrid();
+            AddGridLables();
         }
 
         private void InitializeGrid()
         {
-            Label lable;
-            
+            this.Width = 600;
+            this.Height = 600;
             Grid.BackColor = Color.LightSteelBlue;
             Grid.CellBorderStyle = TableLayoutPanelCellBorderStyle.Outset;
             //Webdings font
-            //e, d, k, b, N, !, &, $
+            //e, d, k, b, N, !, &, $  
+        }
 
+        private void AddGridLables()
+        {
+            Label label;
             for (int c = 0; c < 4; c++)
             {
-                for(int r = 0; r < 4; r++)
+                for (int r = 0; r < 4; r++)
                 {
+                    label = new Label
+                    {
+                        Dock = DockStyle.Fill,
+                        ForeColor = Color.Black,
+                        AutoSize = false,
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Font = new Font("Webdings", 72, FontStyle.Bold),
+                        Text = "e"
 
+                    };
+                    Grid.Controls.Add(label, c, r);
                 }
             }
-
-
-            //Grid.Controls.Add(new Label { Text = "Type:", Anchor = AnchorStyles.Left, AutoSize = true }, 0, 0);
-            //Grid.Controls.Add(new Label { Text = "Type:", Anchor = AnchorStyles.Left, AutoSize = true }, 0, 1);
-            //Grid.Controls.Add(new Label { Text = "Type:", Anchor = AnchorStyles.Left, AutoSize = true }, 1, 0);
-            //Grid.Controls.Add(new Label { Text = "Type:", Anchor = AnchorStyles.Left, AutoSize = true }, 3, 3);
-
         }
 
     }
